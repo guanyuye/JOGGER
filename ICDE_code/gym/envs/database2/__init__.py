3e8049e4e317d72061ff854382351ea03294f598
ygy_UESTC <50436700+guanyuye@users.noreply.github.com>
2022-02-09 23:12:31 +0800

    Delete ICDE_code directory

--- ICDE_code/gym/envs/database2/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from  gym.envs.database2.postgres_card_env_job import Train_Join_Job, Evaluate_Join_Job, Train_Join_Step_Reward 
-#, Train_Join_Step_Tree_Struct
-from gym.envs.database2.postgres_card_env_job_tree import Train_Join_Step_Tree_Struct,Test_Join_Step_Tree_Struct
